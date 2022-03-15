271324e9dda0b6af4f3395a68eeade513ef0ced6
pepyo <pyo5462@ks.ac.kr>
2022-03-15 22:05:10 +0900

    0315

---
+++ 20220315/20220315/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int year = int.Parse(Console.ReadLine());
+
+            if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+                Console.WriteLine("윤년");
+            else
+                Console.WriteLine("평년");
+        }
+    }
+}
