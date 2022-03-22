45750728809833204f92fa6c416d98dc0f5a0020
pepyo <pyo5462@ks.ac.kr>
2022-03-22 22:05:10 +0900

    0322

---
+++ 20220322/20220322/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (i < 10)
+            {
+                i++;
+                if (i % 2 == 0) continue;
+                Console.Write(i +" ");
+            }
+        }
+    }
+}
