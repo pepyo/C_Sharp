4547f9ecd845e5ad3dfe3dff51dba7b796ea850f
pepyo <pyo5462@ks.ac.kr>
2022-03-29 22:05:10 +0900

    0329

---
+++ 20220329/20220329/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void SumAvg(int[] a)
+        {
+            int sum = 0;
+            for (int i = 0; i < a.Length; i++)
+                sum += a[i];
+            Console.WriteLine("합계: " + sum);
+            Console.WriteLine("평균: " + sum / a.Length);
+        }
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5 };
+            SumAvg(array);
+        }
+
+
+    }
+}
