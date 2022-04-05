61524098b272e2032299a4ee32b290adc520fd8e
pepyo <pyo5462@ks.ac.kr>
2022-04-05 22:05:10 +0900

    0405

---
+++ 20220405/20220405/Inheritance.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace InheritanceTest
+{
+    //부모 클래스
+    class Animal
+    {
+        public string name; //이름
+
+        //Animal 클래스 생성자
+        public Animal()
+        {
+            Console.WriteLine($"Animal 생성자");
+        }
+    }
+
+    //자식 클래스
+    class Dog : Animal
+    {
+        public Dog()
+        {
+            Console.WriteLine($"Dog 생성자");
+        }
+
+        public void printDog(string name)
+        {
+            base.name = name;
+            Console.WriteLine($"Dog의 이름은 {name} 입니다.");
+        }
+    }
+
+    //자식 클래스
+    class Cat : Animal
+    {
+        public Cat()
+        {
+            Console.WriteLine($"Cat 생성자");
+        }
+
+        public void printCat(string name)
+        {
+            base.name = name;
+            Console.WriteLine($"Cat의 이름은 {name} 입니다.");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            dog.printDog("뽀삐");
+            Console.WriteLine("--------------");
+
+            Cat cat = new Cat();
+            cat.printCat("여름이");
+        }
+    }
+}
---
+++ 20220405/20220405/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 5;
+            for (int i = 0; i < num; i++)
+            {
+                for (int j = num - 1; j > i; j--)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int j = 0; j < 2 * i + 1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
