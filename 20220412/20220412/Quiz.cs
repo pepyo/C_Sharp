e8b1eec23f5405bec959792125486b6c9f76cd50
pepyo <pyo5462@ks.ac.kr>
2022-04-12 22:05:10 +0900

    0412

---
+++ 20220412/20220412/Quiz.cs
@@ -0,0 +1,29 @@
+﻿public class Solution
+{
+    public bool solution(string s)
+    {
+        Stack<string> stack = new Stack<string>();
+
+        foreach (char word in s)
+        {
+
+            if (word == '(')
+            {
+                stack.Push(word.ToString());
+                continue;
+            }
+
+            
+            if (stack.Count == 0)
+            {
+                return false;
+            }
+
+
+            stack.Pop();
+        }
+
+
+        return stack.Count == 0;
+    }
+}
